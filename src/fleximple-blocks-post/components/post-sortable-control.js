7a2464bba26af58b9853b6e697532043f7b8a213
Rodrigo D’Agostino <rodrigo.dagostino@proton.me>
2026-07-10 17:49:26 -0300

    perf: hoist sortable list components out of render

--- src/fleximple-blocks-post/components/post-sortable-control.js
+++ src/fleximple-blocks-post/components/post-sortable-control.js
@@ -12,96 +12,120 @@ import {
 	toggleAttribute,
 } from './sortable-control';
 
-function PostSortableControl({
-	attributes,
-	attributes: { orderArticle, orderContent, orderMedia, orderMeta },
-	setAttributes,
-}) {
-	const SortableItem = SortableElement(({ value }) => {
-		const label = getLabel(value);
-		let icon = 'hidden';
-		let text = getHelpText(value, 'hidden');
+const SortableItem = SortableElement(({ value, attributes, setAttributes }) => {
+	const label = getLabel(value);
+	let icon = 'hidden';
+	let text = getHelpText(value, 'hidden');
 
-		if (getState(value, attributes)) {
-			icon = 'visibility';
-			text = getHelpText(value, 'visible');
-		}
+	if (getState(value, attributes)) {
+		icon = 'visibility';
+		text = getHelpText(value, 'visible');
+	}
 
-		return (
-			<div className="fleximple-components-sortable-control__item">
-				<DragHandle />
-				<div className="fleximple-components-sortable-control__label">{label}</div>
-				<Button
-					icon={icon}
-					label={text}
-					className="fleximple-components-sortable-control__button"
-					onClick={() => toggleAttribute(value, attributes, setAttributes)}
-				/>
-			</div>
-		);
-	});
+	return (
+		<div className="fleximple-components-sortable-control__item">
+			<DragHandle />
+			<div className="fleximple-components-sortable-control__label">{label}</div>
+			<Button
+				icon={icon}
+				label={text}
+				className="fleximple-components-sortable-control__button"
+				onClick={() => toggleAttribute(value, attributes, setAttributes)}
+			/>
+		</div>
+	);
+});
 
-	const SortableList = SortableContainer(({ items }) => {
-		return (
-			<div className="fleximple-components-sortable-control__sortable-list">
-				{items.map((value, index) => {
-					// Used for non-sortable items
-					// if ( 'featuredImage' === value || 'audio' === value ) {
-					// 	return <Item key={ `item-${ index }` } index={ index } value={ value } />;
-					// }
-					switch (value) {
-						case 'media':
-							return (
-								<>
-									<SortableItem key={`item-${index}`} index={index} value={value} />
-									<SortableList
-										items={orderMedia}
-										onSortStart={onSortStart}
-										onSortEnd={(sortEnd, e) =>
-											onSortEnd(sortEnd, e, 'orderMedia', orderMedia, setAttributes)
-										}
-										useDragHandle
-										helperClass="fleximple-components-sortable-control__helper"
-									/>
-								</>
-							);
-						case 'content':
-							return (
-								<>
-									<SortableItem key={`item-${index}`} index={index} value={value} />
-									<SortableList
-										items={orderContent}
-										onSortStart={onSortStart}
-										onSortEnd={(sortEnd, e) =>
-											onSortEnd(sortEnd, e, 'orderContent', orderContent, setAttributes)
-										}
-										useDragHandle
-										helperClass="fleximple-components-sortable-control__helper"
-									/>
-								</>
-							);
-						case 'meta':
-							return (
-								<>
-									<SortableItem key={`item-${index}`} index={index} value={value} />
-									<SortableList
-										items={orderMeta}
-										onSortStart={onSortStart}
-										onSortEnd={(sortEnd, e) =>
-											onSortEnd(sortEnd, e, 'orderMeta', orderMeta, setAttributes)
-										}
-										useDragHandle
-										helperClass="fleximple-components-sortable-control__helper"
-									/>
-								</>
-							);
-					}
-					return <SortableItem key={`item-${index}`} index={index} value={value} />;
-				})}
-			</div>
-		);
-	});
+const SortableList = SortableContainer(({ items, attributes, setAttributes }) => {
+	return (
+		<div className="fleximple-components-sortable-control__sortable-list">
+			{items.map((value, index) => {
+				switch (value) {
+					case 'media':
+						return (
+							<>
+								<SortableItem
+									key={`item-${index}`}
+									index={index}
+									value={value}
+									attributes={attributes}
+									setAttributes={setAttributes}
+								/>
+								<SortableList
+									items={attributes.orderMedia}
+									onSortStart={onSortStart}
+									onSortEnd={(sortEnd, e) =>
+										onSortEnd(sortEnd, e, 'orderMedia', attributes.orderMedia, setAttributes)
+									}
+									useDragHandle
+									helperClass="fleximple-components-sortable-control__helper"
+									attributes={attributes}
+									setAttributes={setAttributes}
+								/>
+							</>
+						);
+					case 'content':
+						return (
+							<>
+								<SortableItem
+									key={`item-${index}`}
+									index={index}
+									value={value}
+									attributes={attributes}
+									setAttributes={setAttributes}
+								/>
+								<SortableList
+									items={attributes.orderContent}
+									onSortStart={onSortStart}
+									onSortEnd={(sortEnd, e) =>
+										onSortEnd(sortEnd, e, 'orderContent', attributes.orderContent, setAttributes)
+									}
+									useDragHandle
+									helperClass="fleximple-components-sortable-control__helper"
+									attributes={attributes}
+									setAttributes={setAttributes}
+								/>
+							</>
+						);
+					case 'meta':
+						return (
+							<>
+								<SortableItem
+									key={`item-${index}`}
+									index={index}
+									value={value}
+									attributes={attributes}
+									setAttributes={setAttributes}
+								/>
+								<SortableList
+									items={attributes.orderMeta}
+									onSortStart={onSortStart}
+									onSortEnd={(sortEnd, e) =>
+										onSortEnd(sortEnd, e, 'orderMeta', attributes.orderMeta, setAttributes)
+									}
+									useDragHandle
+									helperClass="fleximple-components-sortable-control__helper"
+									attributes={attributes}
+									setAttributes={setAttributes}
+								/>
+							</>
+						);
+				}
+				return (
+					<SortableItem
+						key={`item-${index}`}
+						index={index}
+						value={value}
+						attributes={attributes}
+						setAttributes={setAttributes}
+					/>
+				);
+			})}
+		</div>
+	);
+});
 
+function PostSortableControl({ attributes, attributes: { orderArticle }, setAttributes }) {
 	return (
 		<div className="fleximple-components-sortable-control">
 			<SortableList
@@ -112,6 +136,8 @@ function PostSortableControl({
 				}
 				useDragHandle
 				helperClass="fleximple-components-sortable-control__helper"
+				attributes={attributes}
+				setAttributes={setAttributes}
 			/>
 		</div>
 	);
